b535f5d7f7f815ff2660c384f4b92765fa6d85cf
Титов Илья Александрович <TitovIA@gbucnd.loc>
2019-08-30 10:16:28 +0300

    exemple auth session + add mux

--- routers/indexRoute.go
+++ routers/indexRoute.go
@@ -1,13 +1,25 @@
 package routers
 
 import (
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"net/http"
+	"text/template"
 	"time"
 
 	"../models"
+	"github.com/gorilla/sessions"
+)
+
+var cookieStore = sessions.NewCookieStore([]byte("MySecretKey"))
+
+const cookieName = "Role"
+
+type sesKey int
+
+const (
+	sesKeyLogin sesKey = iota
 )
 
 // logRequest - логгер/принтер в консоль данных о запросе
@@ -20,14 +32,21 @@ func logRequest(r *http.Request) {
 // IndexRoute = is '/'
 // Передача параметров в template
 func IndexRoute(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
+	// logRequest(r)
+	ses, _ := cookieStore.Get(r, cookieName)
+	login, ok := ses.Values[sesKeyLogin].(string)
+	if !ok {
+		fmt.Println("not role")
+	} else {
+		fmt.Println(login)
+	}
 	tmp, _ := template.ParseFiles("./templates/index.html")
 	tmp.Execute(w, "Hello")
 }
 
 // AjaxUsers - 1 query
 func AjaxUsers(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
+	// logRequest(r)
 	w.Header().Set("Content-Type", "application/json")
 	Data := models.User{
 		Name:  "Max",
@@ -35,15 +54,15 @@ func AjaxUsers(w http.ResponseWriter, r *http.Request) {
 		Langs: []string{"RU", "ENG"},
 	}
 	respJSON, _ := json.Marshal(Data)
-	fmt.Fprint(w, string(respJSON))
+	w.Write(respJSON)
 }
 
 // AjaxDepartment - 2 query
 func AjaxDepartment(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
+	// logRequest(r)
 	w.Header().Set("Content-Type", "application/json")
 	Data := models.Department{
-		Name:    "Бухгалтерия",
+		Name:    `Отдел "Бухгалтерия"`,
 		Number:  2,
 		Country: "Russia",
 	}
@@ -53,7 +72,7 @@ func AjaxDepartment(w http.ResponseWriter, r *http.Request) {
 
 // AjaxDB - запрос к БД
 func AjaxDB(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
+	// logRequest(r)
 	q := models.DirectoryValue{}
 	Data := q.GetAll()
 	respJSON, _ := json.Marshal(Data)
@@ -62,9 +81,18 @@ func AjaxDB(w http.ResponseWriter, r *http.Request) {
 
 // AjaxGetOne - выбрать одно значение
 func AjaxGetOne(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
+	// logRequest(r)
 	q := models.DirectoryValue{ID: 2816}
 	Data := q.SelectOne()
 	respJSON, _ := json.Marshal(Data)
 	fmt.Fprint(w, string(respJSON))
+}
+
+// Login - типа авторизация
+func Login(w http.ResponseWriter, r *http.Request) {
+	gob.Register(sesKey(0))
+	ses, _ := cookieStore.Get(r, cookieName)
+	ses.Values[sesKeyLogin] = "User1"
+	cookieStore.Save(r, w, ses)
+	w.Write([]byte("You are loggin"))
 }
--- server.go
+++ server.go
@@ -3,14 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
+
 	"./routers"
 )
 
 func main() {
-	http.HandleFunc("/", routers.IndexRoute)
-	http.HandleFunc("/user", routers.AjaxUsers)
-	http.HandleFunc("/dep", routers.AjaxDepartment)
-	http.HandleFunc("/db", routers.AjaxDB)
-	http.HandleFunc("/getone", routers.AjaxGetOne)
-	http.ListenAndServe("0.0.0.0:9000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", routers.IndexRoute)
+	r.HandleFunc("/user", routers.AjaxUsers)
+	r.HandleFunc("/dep", routers.AjaxDepartment)
+	r.HandleFunc("/login", routers.Login)
+	// http.HandleFunc("/db", routers.AjaxDB)
+	// http.HandleFunc("/getone", routers.AjaxGetOne)
+	http.ListenAndServe("0.0.0.0:9000", r)
 }
