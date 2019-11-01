859536a2302ebbe8f5638ff1167ea88652fbe353
StVoid <stvoid.it@yandex.ru>
2019-11-01 11:12:39 +0300

    worker optimization

--- src/backend/services/monitor.go
+++ src/backend/services/monitor.go
@@ -5,79 +5,83 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 	"time"
 
 	"../store"
 	services "./youtube"
 )
 
-func getURL(link chan store.Video, results chan<- store.Statistic) {
+var logger log.Logger
+
+func init() {
+	logfile, err := os.OpenFile("monitor.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	logger.SetOutput(logfile)
+}
+
+func getURL(link chan store.Video, results chan<- store.Statistic, wg *sync.WaitGroup) {
+	defer wg.Done()
 	tr := &http.Transport{DisableKeepAlives: false}
 	client := &http.Client{Timeout: 600 * time.Second, Transport: tr}
-	for {
-		select {
-		case url := <-link:
-			req, err := http.NewRequest("GET", url.URL, nil)
-			if err != nil {
-				log.Fatalln(url, err)
-			}
-			req.Header.Set("user-agent", "Chrome/78.0.3904.70")
-			response, err := client.Do(req)
-			if err != nil {
-				fmt.Println(url.URL)
-				results <- store.Statistic{}
-				return
-			}
-			b, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				results <- store.Statistic{}
-				return
-			}
-			stat, err := services.ParseYoutube(b)
-			stat.Video = url.ID
-			if err != nil {
-				log.Println(url.URL, err.Error())
-			}
-			results <- stat
-		default:
+	for url := range link {
+		req, err := http.NewRequest("GET", url.URL, nil)
+		if err != nil {
+			logger.Println(url.URL, err.Error())
+		}
+		req.Header.Set("user-agent", "Chrome/78.0.3904.70")
+		response, err := client.Do(req)
+		if err != nil {
 			continue
 		}
+		b, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			continue
+		}
+		stat, err := services.ParseYoutube(b)
+		if err != nil {
+			logger.Println(url.URL, err.Error())
+		}
+		stat.Video = url.ID
+		results <- stat
 	}
-
 }
 
 func worker(urls []store.Video, threads int) []store.Statistic {
 	links := make(chan store.Video, len(urls))
-	results := make(chan store.Statistic)
-
-	for i := 0; i < threads; i++ {
-		go getURL(links, results)
-	}
+	results := make(chan store.Statistic, len(urls))
 
 	for _, url := range urls {
 		links <- url
 	}
 	close(links)
 
+	var wg sync.WaitGroup
+	for i := 0; i < threads; i++ {
+		wg.Add(1)
+		go getURL(links, results, &wg)
+	}
+
+	wg.Wait()
+	close(results)
+
 	var data []store.Statistic
-loop:
 	for {
-		select {
-		case l := <-results:
-			data = append(data, l)
-			if len(data) == len(urls) {
-				break loop
-			}
-		default:
-			continue
+		l, ok := <-results
+		if !ok {
+			break
 		}
+		data = append(data, l)
 	}
 	return data
 }
 
 func main() {
 	all := store.GetAllUrls()
-	data := worker(all, 10)
+	data := worker(all, 4)
 	for _, s := range data {
 		s.Insert()
 	}
